a0a25c4bc9f21e07eab003c3cd2fad0fa5c4e1f9
Drkayal <faresbakil7734@gmail.com>
2025-08-07 02:30:18 +0300

    Update OWNER.py

--- OWNER.py
+++ OWNER.py
@@ -1,23 +1,23 @@
 # متطلبات التنصيب #
-OWNER = ["Y_o_V"] 
-OWNER_ID = [6094238403]
+OWNER = ["AAAKP"] 
+OWNER_ID = [985612253]
 #اسم المطور
-OWNER_NAME = "𓏺 َ𝗬𝗼𝘂𝘀𝗲𝗳"
+OWNER_NAME = "𓏺 َ𝐷𝑟. 𝐾ℎ𝑎𝑦𝑎𝑙 𓏺"
 #اسم التي سيظهر على صورة
-infophoto = "Alsayed Playing"
+infophoto = "Source LoL"
 #توكن
-BOT_TOKEN = "7585465236:AAFnughsD8VmGUdIkpVj7pydN0MJnpvz_Qk"
+BOT_TOKEN = ""
 #مونجو
-DATABASE = "mongodb+srv://huSeen96:Huseenslah96@cluster0.ld2v7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" 
+DATABASE = "8020231543:AAHFLDS8a77eqGA045YZFV8uQjvr34IvWrM/?retryWrites=true&w=majority&appName=Cluster0" 
 #قناة سورس
-CHANNEL = "https://t.me/cecrr"
+CHANNEL = "https://t.me/k55dd"
 #كروب سورس
-GROUP = "https://t.me/PRTRZA"
+GROUP = "https://t.me/YMMYN"
 #فيديو سورس اذا ماعندك خليه
 VIDEO = "https://t.me/MusicxXxYousef/91"
 #صورة سورس اذا ماعندك خليها
 PHOTO = "https://t.me/MusicxXxYousef/90"
 #ساوي كروب وضيف عليه مصنع ورفعو اشراف وحط يوزر الكروب هنا
-LOGS = "xjjfjfhh"
+LOGS = "Yemen_Chat2"
  
  
