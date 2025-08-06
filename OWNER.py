8e46941bf9f4216b5e3e734fcac3f905465d1067
Drkayal <faresbakil7734@gmail.com>
2025-08-07 02:37:55 +0300

    Update OWNER.py

--- OWNER.py
+++ OWNER.py
@@ -6,7 +6,7 @@ OWNER_NAME = "𓏺 َ𝐷𝑟. 𝐾ℎ𝑎𝑦𝑎𝑙 𓏺"
 #اسم التي سيظهر على صورة
 infophoto = "Source LoL"
 #توكن
-BOT_TOKEN = ""
+BOT_TOKEN = "8020231543:AAHFLDS8a77eqGA045YZFV8uQjvr34IvWrM"
 #مونجو
 DATABASE = "mongodb+srv://fasd2025:faresbakil2025@cluster0.wdjssoo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 #قناة سورس
